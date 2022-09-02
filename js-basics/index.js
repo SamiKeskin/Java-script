5000db8072b627c52ac218e62936225b3f0ccbee
unknown <Sami cool@MSI-A01333.mps.k12>
2022-09-02 12:05:29 -0800

    second day of learning java script

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,48 +1,6 @@
-console.log('Hello World');
-
-let name = 'Sami';
-let age = 30;
-let isApproved = false;
-let firstName = undefined;
-let selectedColor = null;
-console.log(name, age, isApproved, firstName, selectedColor);
-
-// Cannot be a reserved keyword
-//Should be meaningful
-//Cannot start with a number (1name)
-// Cannot contain a space or a hyphen (-)
-// Are case-sensitive
-
-let firstName ='Sami';
-let lastname = 'keskin';
-
-const interestRate = 0.3;
-interestRate = 1;
+let person = {
+    name: 'Mosh', age: 30
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(interestrate);
+console.log(person)
