b8bcced36137aaa1f3d052cbd79711a984ce718e
unknown <Sami cool@MSI-A01333.mps.k12>
2022-09-13 12:05:51 -0800

    finished 5th day of learning javascript

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,6 +1,25 @@
-function power(base, ez){
-    console.log(base ** ez);
+/*
+let counter = 1;
+
+while(counter < 100){
+    console.log(counter)
+    counter++
 }
 
-power(2, 8);
-power(2, 10000000000);
+
+for (let i = 1; i < 101 ; i++){
+    console.log(i)
+}
+*/
+alert ("Acess Granted!!")
+
+let password="magnolia123"
+let userInput = null
+while(userInput =! password){
+    userInput = prompt("Please neter the right password")
+}
+
+
+
+let myName = prompt("Please say your name")
+alert("Welcome " + myName + " to your website")
